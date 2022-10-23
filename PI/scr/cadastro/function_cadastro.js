ab1f4618cc9a205ec319a1f17f6f46b2db1f5251
MatheusLTrindade <matheus.lopes.trindade@hotmail.com>
2022-10-23 18:48:27 -0300

    Finish home page

--- PI/scr/cadastro/function_cadastro.js
+++ PI/scr/cadastro/function_cadastro.js
@@ -35,6 +35,10 @@ function mask(){
       $(this).mask('000.000.000-00');
    });
 
+   $("#birth-date").keypress(function() {
+      $(this).mask('00/00/0000');
+   });
+
    $("#cep").keypress(function() {
       $(this).mask('00000-000');
    });
