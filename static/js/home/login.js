827595e7b7f0a62df6cf6c92077280aeee49c1c1
Murilolima98 <murilolimaa1998@gmail.com>
2022-11-11 18:18:37 -0300

    subindo login

---
+++ static/js/home/login.js
@@ -0,0 +1,37 @@
+function onlynumber(e) {
+    var Evento = e || window.event ;
+    var key = Evento.keyCode || Evento.which;
+    key = String.fromCharCode(key);
+    var regex = /^[0-9]+$/ ;
+    if (!regex.test(key)) {
+        Evento.returnValue = false ;
+        if (Evento.preventDefault) {
+            Evento.preventDefault ();
+        }
+
+    }
+
+
+}
+
+function mask(){
+    $("#cpf").keypress(function() {
+       $(this).mask('000.000.000-00');
+    });
+    
+    
+    
+}
+
+
+function verificar() { 
+    
+    let senha = document.getElementById("senha").value
+  
+    // alert(senha)
+      if (senha.length < 10) {
+        alert( " senha incorreta")
+
+    }
+}
+    
