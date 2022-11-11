aeac93b4e34d04484d43e7d41ea8b2e529f28b68
MatheusLTrindade <matheus.lopes.trindade@hotmail.com>
2022-11-10 21:12:59 -0300

    config heroku

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -27,4 +28,5 @@ def extrato():
     return render_template("./user/extrato.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv('PORT'), '5000')
+    app.run(host='0.0.0.0', port = port)
