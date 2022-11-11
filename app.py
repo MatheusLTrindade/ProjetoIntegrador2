5fe0e9cbfd6a650807de9fa48b815ee79831e6d9
MatheusLTrindade <matheus.lopes.trindade@hotmail.com>
2022-11-11 02:07:44 -0300

    adjusting recursion of user dashboard

--- app.py
+++ app.py
@@ -28,5 +28,5 @@ def extrato():
     return render_template("./user/extrato.html")
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT'), '5000')
-    app.run(host='0.0.0.0', port = port)
+    # port = int(os.getenv('PORT'), '5000')
+    app.run(host='0.0.0.0', port = 8080)
